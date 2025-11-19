a897a2c2cb89fba8c2ce44d5a93b139d8a243b17
Amelia <ibarguen.luz@correounivalle.edu.co>
2025-11-19 11:41:43 -0500

    Corrigiendo rutas diccionario/municipio #1.7

--- Airsense-V2/tailwind.config.js
+++ Airsense-V2/tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-  "./public/visor.html",  // Escanea la nueva página de inicio
-  "./public/js/rutas.js",    // Escanea el JS de la inicio
-  "./public/mapa.html",   // Escanea la página del mapa
-  "./public/js/script.js"    // Escanea el JS del mapa
-],
+    "./public/**/*.{html,js}",
+    "./src/**/*.{html,js}"
+  ],
   theme: {
     extend: {},
   },
